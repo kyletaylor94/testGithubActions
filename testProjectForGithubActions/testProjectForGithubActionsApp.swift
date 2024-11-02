46fca0ed1af2bfc148c0a932f3456f1ac5cdeeb4
T. Csaba <kyletaylorrules94@gmail.com>
2024-11-02 20:39:34 +0100

    initial commit

---
+++ testProjectForGithubActions/testProjectForGithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectForGithubActionsApp.swift
+//  testProjectForGithubActions
+//
+//  Created by Turdesan Csaba on 2024. 11. 02..
+//
+
+import SwiftUI
+
+@main
+struct testProjectForGithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
